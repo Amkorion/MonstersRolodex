80b31807cc6849603f09c2bd9e3cea87e4ada33d
Oleksandr Moroz <morozlex@gmail.com>
2022-05-05 15:21:19 +0300

    Functioanal components app js page done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [searchField, setSearchField] = useState("");
-  console.log({ searchField });
+  const [monsters, setMonsters] = useState([]);
+  let [filteredMonsters, setFilterMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
@@ -19,7 +35,7 @@ const App = () => {
         placeholder="Search Monsters"
         className="monsters-search-box"
       />
-      {/*<CardList monsters={filteredMonsters} />*/}
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 };
