c948911f6725e0951df2f09599b6de3c2632c50b
Oleksandr Moroz <morozlex@gmail.com>
2022-05-06 10:00:54 +0300

    +text input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 
 const App = () => {
   const [searchField, setSearchField] = useState("");
+  const [title, setTitle] = useState("");
   const [monsters, setMonsters] = useState([]);
   let [filteredMonsters, setFilterMonsters] = useState(monsters);
 
@@ -26,10 +27,23 @@ const App = () => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
+  const onTitleChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString);
+  };
 
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
+      <h3 className="app-writing">{title}</h3>
+      <SearchBox
+        onChangeHandler={onTitleChange}
+        placeholder="Write something"
+        className="monsters-search-box"
+      />
+
+      <br />
+
       <SearchBox
         onChangeHandler={onSearchChange}
         placeholder="Search Monsters"
